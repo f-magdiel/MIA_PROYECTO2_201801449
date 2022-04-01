1edf574b94a3b601038966ac4dd360029e7e9171
magdiel <3275257541405@ingenieria.usac.edu.gt>
2022-04-01 15:32:50 -0600

    Menu

---
+++ Main.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strings"
+)
+
+func main() {
+	salir := true
+	for salir {
+		fmt.Println("-----------------------------------------")
+		fmt.Println("|  PROYECTO 2                           |")
+		fmt.Println("|  MANEJO E IMPLEMETANCION DE ARCHIVOS  |")
+		fmt.Println("|  FRANCISCO MAGDIEL ASICONA MATEO      |")
+		fmt.Println("|  PRIMER SEMESTRE 2022                 |")
+		fmt.Println("------------------------------------------")
+		fmt.Print("COMANDO >> ")
+		lector := bufio.NewReader(os.Stdin)   // para leer la cadena de string
+		comando, _ := lector.ReadString('\n') // lee cada caracter
+		if strings.Compare(comando, "salir") == 0 {
+			salir = false
+			fmt.Println("Se detuvo el programa...")
+		} else {
+			fmt.Println("analizando....")
+		}
+	}
+
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module MIA_PROYECTO2_201801449
+
+go 1.18
