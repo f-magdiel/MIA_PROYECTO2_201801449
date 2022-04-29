00e7ceac792cd3d7cf2bc12f7e1c81c3ea8d8bca
magdiel <3275257541405@ingenieria.usac.edu.gt>
2022-04-28 21:08:36 -0600

    struct ebr and module fdisk

---
+++ Fdisk.go
@@ -0,0 +1,5 @@
+package main
+
+func AnalisisFdisk(comando string) {
+
+}
---
+++ ebr.go
@@ -0,0 +1,10 @@
+package main
+
+type Ebr struct {
+	Part_status byte
+	Part_fit    [2]byte
+	Part_start  int64
+	Part_size   int64
+	Part_next   int64
+	Part_name   [16]byte
+}
